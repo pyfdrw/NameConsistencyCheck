505c32578b9ee13a8f1000c802eec712a365b328
pyf <tobepyf@hotmail.com>
2019-08-28 10:06:51 +0800

    添加项目文件。

---
+++ NameConsistencyCheck.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using VMS.TPS.Common.Model.API;
+using VMS.TPS.Common.Model.Types;
+
+namespace NameConsistencyCheck
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (Application app = Application.CreateApplication(null, null))
+                {
+                    Execute(app);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(e.ToString());
+            }
+        }
+        static void Execute(Application app)
+        {
+            // TODO: add here your code
+        }
+    }
+}
